5759fd13b8ec3e5642caa3627abf409d8560490a
Seaaal <Seaaal@users.noreply.github.com>
2014-08-03 15:47:05 +0100

    Added Required Files

---
+++ Matt-Mih-Helper/Main.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Matt_Mih_Helper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbChampions1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbChampions1
+            // 
+            this.cbChampions1.FormattingEnabled = true;
+            this.cbChampions1.Location = new System.Drawing.Point(95, 49);
+            this.cbChampions1.Name = "cbChampions1";
+            this.cbChampions1.Size = new System.Drawing.Size(121, 21);
+            this.cbChampions1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Champion:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbChampions1);
+            this.Name = "Main";
+            this.Text = "Mentored Inhouse Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbChampions1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
