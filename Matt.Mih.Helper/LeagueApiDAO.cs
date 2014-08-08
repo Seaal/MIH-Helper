d1bf731f54cf59103881c602c3dc52a0526a160f
Seaaal <Seaaal@users.noreply.github.com>
2014-08-08 22:24:10 +0100

    Adding Missing Files: LeagueApiDAO and JsonConvert Library

---
+++ Matt.Mih.Helper/LeagueApiDAO.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matt.Mih.Helper
+{
+    class LeagueApiDAO
+    {
+        static readonly string API_KEY = "63965c03-965b-4cfb-bb3e-e8b0a0601bd1";
+
+        public ChampionDTO GetChampions()
+        {
+            string json = MakeRequest("static-data/na/v1.2/champion");
+
+            return JsonConvert.DeserializeObject<ChampionDTO>(json);
+        }
+
+        public Summoner GetSummoner(string summonerName)
+        {
+            string json = MakeRequest("na/v1.4/summoner/by-name/"+summonerName);
+
+            Dictionary<string, Summoner> sumDto = JsonConvert.DeserializeObject<Dictionary<string, Summoner>>(json);
+
+            return sumDto.FirstOrDefault().Value;
+        }
+
+
+        private string MakeRequest(string resource)
+        {
+            String urlRequest = "https://na.api.pvp.net/api/lol/" + resource + "?api_key=" + API_KEY;
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(urlRequest);
+            Stream response = null;
+
+            // Sends the HttpWebRequest and waits for the response.
+            HttpWebResponse myHttpWebResponse = myReq.GetResponse() as HttpWebResponse;
+            try
+            {
+                response = myHttpWebResponse.GetResponseStream();
+                StreamReader readStream = new StreamReader(response, System.Text.Encoding.GetEncoding("utf-8"));
+                return readStream.ReadToEnd();
+            }
+            finally
+            {
+                // Releases the resources of the response.
+                response.Dispose();
+                myHttpWebResponse.Close();
+            }
+        }
+    }
+}
